20a46ee17e6eb69c521ad782162c6efe81de4fb6
1B16AM <114579412+AntonMaksimau@users.noreply.github.com>
2022-10-05 11:19:51 +0000

    Dodano 4 zadania do karty pracy 2

--- KartaPracy2.py
+++ KartaPracy2.py
@@ -1,6 +1,35 @@
-#Zad 1
-wiek = int(input())
-if wiek<18:
-  print("No")
+# Zad 1
+# a = int(input("Podaj liczbę "))
+# if a % 3 == 0:
+#     print("TAK")
+# else:
+#     print("NIE")
+
+# Zad 2
+# a = int(input())
+# if a>=100 and a<1000 and a%17==0:
+#     print("TAK")
+# else:
+#     print("NIE")
+
+# Zad 3
+# wiek = int(input())
+# if wiek<18:
+#   print("No")
+# else:
+#   print("Yes")
+
+# Zad 4
+LIMIT = 20
+waga = int(input())
+if waga<=LIMIT:
+    print("jedz")
 else:
-  print("Yes")
+    print("stop")
+
+# Zad 5
+# a,b,c = int(input()),int(input()),int(input())
+# if c<a and c>b or c>b and c<a:
+#     print("TAK")
+# else:
+#     print("NIE")
