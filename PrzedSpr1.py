7afb8d460ff996d233e9b269b624c970c8731110
1B16AM <114579412+AntonMaksimau@users.noreply.github.com>
2022-11-03 19:16:08 +0000

    PrzedSpr1

--- PrzedSpr1.py
+++ PrzedSpr1.py
@@ -18,17 +18,29 @@
 # print(suma)
 
 # Zad 3 - Hard - Sprawdź czy liczba wpisana przez usera jest doskonałą
-
 # n = int(input())
 # suma = 0
 # for i in range(1,n):
 #     if n % i == 0:
-#         print(i)
 #         suma += i
 # if suma == n:
 #     print("Tak")
 # else:
 #     print("Nie")
+
+# Zad 3.2
+# n = 0
+# while True:
+#     suma = 0
+#     n += 1
+#     print(n)
+#     for i in range(1,n):
+#         if n % i == 0:
+#             suma += i
+#     if suma == n:
+#         print("Tak\n")
+#     else:
+#         print("Nie\n")
 
 # Zad 4 - Medium - Oblicz sumnę liczb dwucyfrowych podzielnych przez 7
 # suma = 0
