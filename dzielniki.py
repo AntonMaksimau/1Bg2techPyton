59319a6cd09551f9addd96ac948ca8e10ee88968
1B16AM <114579412+AntonMaksimau@users.noreply.github.com>
2022-11-14 12:21:13 +0000

    dzielniki

---
+++ dzielniki.py
@@ -0,0 +1,14 @@
+# n = int(input())
+# for i in range(1, n + 1):
+#     if n % i == 0:
+#         print(i)
+
+# n = int(input())
+# licznik = True
+# for i in range(2,n):
+#     if n % i == 0:
+#         licznik = False
+# if licznik:
+#     print("Pierwsza")
+# else:
+#     print("Nie pierwsza")
