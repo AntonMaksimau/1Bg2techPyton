875faf1c0513ecb5018537b73bc712f7087ec9db
1B16AM <114579412+AntonMaksimau@users.noreply.github.com>
2022-11-28 12:05:04 +0000

    Tworzenie algorytmów

--- KartyPracy/Algorytmy1/Pierwsze.py
+++ KartyPracy/Algorytmy1/Pierwsze.py
@@ -1,7 +1,35 @@
-# Sprawdź czy dana liczba jest pierwsza
+# 1. Sprawdź czy dana liczba jest pierwsza
 # n = int(input())
 # for i in range(2,n):
 #     if n % i == 0:
 #         print("Nie jest pierwszą")
-#         exit()
+#         exit(0)
 # print("Tak, jest pierwszą")
+
+# 2. Generowanie liczb pierwszych (w przedziale [p,q])
+# p, q = int(input()), int(input())
+# for j in range(p, q + 1):
+#     flaga = True
+#     for i in range(2, j):
+#         if j % i == 0:
+#             flaga = False
+#             break
+#     if flaga:
+#         print(j, end=" ")
+
+# 3. Generowanie liczb pierwszych (pierwsze n liczb pierwszych)
+# n = int(input("Ile liczb pierwszych wypisać? "))
+# k = 2
+# ilosc = 0
+# while 1:
+#     flaga = True
+#     for i in range(2, k):
+#         if k % i == 0:
+#             flaga = False
+#             break
+#     if flaga:
+#         print(k, end=" ")
+#         ilosc += 1
+#     if ilosc == n:
+#         break
+#     k += 1
