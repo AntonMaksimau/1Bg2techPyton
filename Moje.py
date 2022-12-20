f58d465a4edf93863f2d3d99ab958c83ccc19dbb
1B16AM <114579412+AntonMaksimau@users.noreply.github.com>
2022-12-20 21:16:14 +0000

    Moje

--- Moje.py
+++ Moje.py
@@ -1,66 +1,66 @@
 ### 1.0
-m = 0
-while m != 1 and m != 2:
-    m = str(input("Policzyć liczbę konkretną(1) czy wszystkie liczby do wpisanej(2)? "))
-    if m == "1":
-        m1 = int(input("\nKtórą liczbę policzyć? "))
-        suma = 0
-        for i in range(1, m1 // 2 + 1):
-            if m1 % i == 0:
-                suma += i
-        if suma == m1:
-            print(f"\nLiczba {m1} jest liczbą doskonałą")
-        else:
-            print(f"\nLiczba {m1} nie jest liczbą doskonałą")
-        break
-    elif m == "2":
-        m2 = int(input("\nDo której liczby policzyć? "))
-        progr = 0
-        while progr != 1 and progr != 2:
-            progr = int(input("\nWypisywać progres za pomocą procentów(1) czy wyświetlania sprawdzenia dla każdej liczby osobno(2)? "))
-            if progr != 1 and progr != 2:
-                print("Można wpisać tylko 1 lub 2!")
-        n = 0
-        ilosc = 0
-        proc = 0
-        list = ""
-        while n < m2:
-            suma = 0
-            check = proc
-            n += 1
-            if progr == 1:
-                for i in range(1,101):
-                    if n >= (i / 100) * m2:
-                        proc = i
-                if proc != check:
-                    print(f"{proc}%")
-            else:
-                print(n)
-            for i in range(1, n // 2 + 1):
-                if n % i == 0:
-                    suma += i
-            if suma == n:
-                if progr == 2:
-                    print("Tak\n")
-                list += "\n" + str(ilosc + 1) + ") " + str(n)
-                ilosc += 1
-            else:
-                if progr == 2:
-                    print("Nie\n")
-        końcówka = ""
-        kon = ""
-        kon2 = "ych"
-        if n % 10 == 1 and n % 100 != 11:
-            końcówka = "y"
-        if ilosc % 10 == 1 and ilosc % 100 != 11:
-            kon = "a"
-            kon2 = kon
-        if (ilosc % 10 == 2 or ilosc % 10 == 3 or ilosc % 10 == 4) and ilosc != 12 and ilosc != 13 and ilosc != 14:
-            kon = "y"
-        print(f"Wśród {n} liczb{końcówka} jest {ilosc} liczb{kon} doskonał{kon2}:\n{list}")
-        break
-    else:
-        print("\nMusiała być wpisana tylko cyfra 1 lub 2!\n")
+# m = 0
+# while m != 1 and m != 2:
+#     m = str(input("Policzyć liczbę konkretną(1) czy wszystkie liczby do wpisanej(2)? "))
+#     if m == "1":
+#         m1 = int(input("\nKtórą liczbę policzyć? "))
+#         suma = 0
+#         for i in range(1, m1 // 2 + 1):
+#             if m1 % i == 0:
+#                 suma += i
+#         if suma == m1:
+#             print(f"\nLiczba {m1} jest liczbą doskonałą")
+#         else:
+#             print(f"\nLiczba {m1} nie jest liczbą doskonałą")
+#         break
+#     elif m == "2":
+#         m2 = int(input("\nDo której liczby policzyć? "))
+#         progr = 0
+#         while progr != 1 and progr != 2:
+#             progr = int(input("\nWypisywać progres za pomocą procentów(1) czy wyświetlania sprawdzenia dla każdej liczby osobno(2)? "))
+#             if progr != 1 and progr != 2:
+#                 print("Można wpisać tylko 1 lub 2!")
+#         n = 0
+#         ilosc = 0
+#         proc = 0
+#         list = ""
+#         while n < m2:
+#             suma = 0
+#             check = proc
+#             n += 1
+#             if progr == 1:
+#                 for i in range(1,101):
+#                     if n >= (i / 100) * m2:
+#                         proc = i
+#                 if proc != check:
+#                     print(f"{proc}%")
+#             else:
+#                 print(n)
+#             for i in range(1, n // 2 + 1):
+#                 if n % i == 0:
+#                     suma += i
+#             if suma == n:
+#                 if progr == 2:
+#                     print("Tak\n")
+#                 list += "\n" + str(ilosc + 1) + ") " + str(n)
+#                 ilosc += 1
+#             else:
+#                 if progr == 2:
+#                     print("Nie\n")
+#         końcówka = ""
+#         kon = ""
+#         kon2 = "ych"
+#         if n % 10 == 1 and n % 100 != 11:
+#             końcówka = "y"
+#         if ilosc % 10 == 1 and ilosc % 100 != 11:
+#             kon = "a"
+#             kon2 = kon
+#         if (ilosc % 10 == 2 or ilosc % 10 == 3 or ilosc % 10 == 4) and ilosc != 12 and ilosc != 13 and ilosc != 14:
+#             kon = "y"
+#         print(f"Wśród {n} liczb{końcówka} jest {ilosc} liczb{kon} doskonał{kon2}:\n{list}")
+#         break
+#     else:
+#         print("\nMusiała być wpisana tylko cyfra 1 lub 2!\n")
 
 
 
@@ -197,3 +197,46 @@ while m != 1 and m != 2:
 #             mian /= i
 #     print(f"{list}{int(mian)}/{int(nww)}")
 
+### 7.1
+x, y = str(input()), str(input())
+a,b, c,d = 0,0, 0,0
+sl = True
+for i in range(len(x)):
+    if x[i] != "/" and sl:
+        a = a * 10 + int(x[i])
+    elif x[i] == "/":
+        sl = False
+    else:
+        b = b * 10 + int(x[i])
+sl = True
+for i in range(len(y)):
+    if y[i] != "/" and sl:
+        c = c * 10 + int(y[i])
+    elif y[i] == "/":
+        sl = False
+    else:
+        d = d * 10 + int(y[i])
+if b == 0:
+    b = 1
+if d == 0:
+    d = 1
+up = int(a * c)
+down = int(b * d)
+for i in range(min(up,down),1,-1):
+    if up % i == 0 and down % i == 0:
+        up = int(up / i)
+        down = int(down / i)
+if up > down:
+    cal = int(up // down)
+    up = int(up - (up // down) * down)
+    if up != 0:
+        print(f"{cal} i {up}/{down}")
+    else:
+        print(f"{cal}")
+elif up == down and up != 0:
+    print("1")
+else:
+    if up != 0:
+        print(f"{up}/{down}")
+    else:
+        print("0")
